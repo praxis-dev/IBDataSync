0c28847e1ffa3dea10220e4c374f11cf1a050149
praxis-dev <chesnokovprm@gmail.com>
2024-04-14 14:06:59 -0600

    feat: basic setup finalized

---
+++ ibapi-service/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+
+app = FastAPI()
+
+class IBapi(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/check-ibapi")
+def check_ibapi():
+    app = IBapi()
+    return {"status": "IBapi initialized successfully"}
