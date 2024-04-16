6bfb712110036fc7ee952f366787d13bb59e86e4
praxis-dev <chesnokovprm@gmail.com>
2024-04-16 14:18:22 -0600

    feat: adjustments

--- ibapi-service/app/main.py
+++ ibapi-service/app/main.py
@@ -10,7 +10,7 @@ class IBapi(EWrapper, EClient):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Worldddd12"}
+    return {"Hello": "Worldddd"}
 
 @app.get("/check-ibapi")
 def check_ibapi():
