e72e2c15c09cff55800f6029a01cbcce0465d948
praxis-dev <chesnokovprm@gmail.com>
2024-04-17 14:28:34 -0600

    feat: ibapi modularized

--- ibapi-service/app/main.py
+++ ibapi-service/app/main.py
@@ -2,16 +2,7 @@ from fastapi import FastAPI
 
 from .api.routes import router as api_router
 from .api.websocket import router as ws_router
-from .core.config import settings
-
-from ibapi.client import EClient
-from ibapi.wrapper import EWrapper
-from datetime import datetime
-from zoneinfo import ZoneInfo
-from threading import Thread
-import asyncio
-from starlette.websockets import WebSocketState
-
+from .services.ibapi_service import IBapi
 
 app = FastAPI()
 
@@ -27,85 +18,6 @@ def serialize_contract(contract):
         "exchange": contract.exchange
     }
 
-class IBapi(EWrapper, EClient):
-    def __init__(self):
-        EClient.__init__(self, self)
-        self.executions = []
-        self.orders = []
-        self.observers = []
-
-    async def notify_observers(self, message):
-        for observer in self.observers:
-            if observer.client_state == WebSocketState.CONNECTED:
-                try:
-                    await observer.send_json(message)
-                except RuntimeError as e:
-                    print(f"Error sending message: {e}")
-
-    def schedule_notification(self, message):
-        loop = asyncio.get_event_loop()
-        if loop.is_running():
-            loop.create_task(self.notify_observers(message))
-        else:
-            loop.run_until_complete(self.notify_observers(message))
-
-
-
-    def execDetails(self, reqId, contract, execution):
-        print("Order Executed:", execution.execId, execution.acctNumber, execution.side, execution.shares, execution.price)
-        self.executions.append(execution)
-        execution_data = {"type": "execution", "data": {
-            "execId": execution.execId,
-            "acctNumber": execution.acctNumber,
-            "side": execution.side,
-            "shares": execution.shares,
-            "price": execution.price
-        }}
-        self.schedule_notification(execution_data)
-
-    def connect_and_start(self):
-        self.connect(settings.ib_host, settings.ib_port, clientId=settings.ib_client_id)
-        thread = Thread(target=self.run_thread, daemon=True)
-        thread.start()
-
-    def run_thread(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        self.run()
-
-    def openOrder(self, orderId, contract, order, orderState):
-            current_time = datetime.now(ZoneInfo(settings.timezone)).strftime('%Y-%m-%d %H:%M:%S')
-            print(f"Order Submitted at {current_time}:", orderId, contract.symbol, order.action, order.totalQuantity, order.lmtPrice)
-            order_info = {
-                "orderId": orderId,
-                "contract": serialize_contract(contract),  
-                "order": {
-                    "action": order.action,
-                    "totalQuantity": order.totalQuantity,
-                    "lmtPrice": order.lmtPrice
-                },
-                "orderState": {
-                    "status": orderState.status
-                },
-                "submissionTime": current_time
-            }
-            if not any(o["orderId"] == orderId for o in self.orders):
-                self.orders.append(order_info)
-            self.schedule_notification({"type": "order", "data": order_info})
-
-
-    def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
-        print("Order Status:", orderId, status, filled)
-        for order in self.orders:
-            if order["orderId"] == orderId:
-                order.update({
-                    "status": status,
-                    "filled": filled,
-                    "remaining": remaining,
-                    "avgFillPrice": avgFillPrice,
-                    "lastFillPrice": lastFillPrice
-                })
-                self.schedule_notification({"type": "order_update", "data": order})
 
 @app.on_event("startup")
 async def startup_event():
