3a45516e7deaffcd0659e7f4afbcbb948d8f5c70
praxis-dev <chesnokovprm@gmail.com>
2024-04-18 19:21:58 -0600

    feat: intercontainer comms progress

--- sentinel/cmd/server/main.go
+++ sentinel/cmd/server/main.go
@@ -1,15 +1,50 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
 
+var (
+	wsURL = "ws://localhost:5000/ws"
+)
+
 func main() {
-    e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World from Go")
-    })
-    e.Logger.Fatal(e.Start(":8080"))
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World from Go")
+	})
+
+	go startWebSocketClient()
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func startWebSocketClient() {
+	dialer := websocket.Dialer{
+		HandshakeTimeout: 45 * time.Second,
+	}
+
+	conn, _, err := dialer.Dial(wsURL, nil)
+	if err != nil {
+		log.Fatal("Error connecting to WebSocket server:", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("Connected to WebSocket server")
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading from WebSocket:", err)
+			return
+		}
+		log.Printf("Received message: %s\n", message)
+	}
 }
--- sentinel/go.mod
+++ sentinel/go.mod
@@ -3,7 +3,11 @@ module sentinel
 go 1.18
 
 require (
-	github.com/labstack/echo/v4 v4.11.4 // indirect
+	github.com/gorilla/websocket v1.4.2
+	github.com/labstack/echo/v4 v4.11.4
+)
+
+require (
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
