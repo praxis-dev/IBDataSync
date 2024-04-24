42e6152fa0d740b1d99dc7196cebcc875d582467
praxis-dev <chesnokovprm@gmail.com>
2024-04-23 20:39:23 -0600

    feat: stuck orders fixed

--- ibapi-service/app/services/ibapi_service.py
+++ ibapi-service/app/services/ibapi_service.py
@@ -1,12 +1,11 @@
 # services/ibapi_service.py
-
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from threading import Thread
 import asyncio
-from ..core.config import settings  
+from ..core.config import settings
 from .notification_service import NotificationService
 
 def serialize_contract(contract):
@@ -21,22 +20,21 @@ class IBapi(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
         self.executions = []
-        self.orders = []
+        self.orders = {}
         self.observers = []
         self.notifier = NotificationService(self.observers)
 
-
-    # def execDetails(self, reqId, contract, execution):
-    #     print("Order Executed:", execution.execId, execution.acctNumber, execution.side, execution.shares, execution.price)
-    #     self.executions.append(execution)
-    #     execution_data = {"type": "execution", "data": {
-    #         "execId": execution.execId,
-    #         "acctNumber": execution.acctNumber,
-    #         "side": execution.side,
-    #         "shares": execution.shares,
-    #         "price": execution.price
-    #     }}
-    #     self.notifier.schedule_notification(execution_data)
+    def execDetails(self, reqId, contract, execution):
+        print("Order Executed:", execution.execId, execution.acctNumber, execution.side, execution.shares, execution.price)
+        self.executions.append(execution)
+        execution_data = {"type": "execution", "data": {
+            "execId": execution.execId,
+            "acctNumber": execution.acctNumber,
+            "side": execution.side,
+            "shares": execution.shares,
+            "price": execution.price
+        }}
+        self.notifier.schedule_notification(execution_data)
 
     def connect_and_start(self):
         self.connect(settings.ib_host, settings.ib_port, clientId=settings.ib_client_id)
@@ -49,35 +47,33 @@ class IBapi(EWrapper, EClient):
         self.run()
 
     def openOrder(self, orderId, contract, order, orderState):
-            current_time = datetime.now(ZoneInfo(settings.timezone)).strftime('%Y-%m-%d %H:%M:%S')
-            print(f"Order Submitted at {current_time}:", orderId, contract.symbol, order.action, order.totalQuantity, order.triggerPrice)
-            order_info = {
-                "orderId": orderId,
-                "contract": serialize_contract(contract),  
-                "order": {
-                    "action": order.action,
-                    "orderType": order.orderType,   
-                    "orderAuxPrice": order.auxPrice,    
-                    "totalQuantity": order.totalQuantity,
-                },
-                "orderState": {
-                    "status": orderState.status
-                },
-                "submissionTime": current_time
-            }
-            if not any(o["orderId"] == orderId for o in self.orders):
-                self.orders.append(order_info)
+        current_time = datetime.now(ZoneInfo(settings.timezone)).strftime('%Y-%m-%d %H:%M:%S')
+        print(f"Order Submitted at {current_time}:", orderId, contract.symbol, order.action, order.totalQuantity, order.lmtPrice)
+        order_info = {
+            "orderId": orderId,
+            "contract": serialize_contract(contract),
+            "order": {
+                "action": order.action,
+                "totalQuantity": order.totalQuantity,
+                "lmtPrice": order.lmtPrice
+            },
+            "orderState": {
+                "status": orderState.status
+            },
+            "submissionTime": current_time
+        }
+        self.orders[orderId] = order_info
+        self.notifier.schedule_notification({"type": "order", "data": order_info})
 
     def orderStatus(self, orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
         print("Order Status:", orderId, status, filled)
-        for order in self.orders:
-            if order["orderId"] == orderId:
-                order.update({
-                    "permId": permId,
-                    "status": status,
-                    "filled": filled,
-                    "remaining": remaining,
-                    "avgFillPrice": avgFillPrice,
-                    "lastFillPrice": lastFillPrice
-                })
-                self.notifier.schedule_notification({"type": "order_update", "data": order})
+        if orderId in self.orders:
+            order = self.orders[orderId]
+            order.update({
+                "status": status,
+                "filled": filled,
+                "remaining": remaining,
+                "avgFillPrice": avgFillPrice,
+                "lastFillPrice": lastFillPrice
+            })
+            self.notifier.schedule_notification({"type": "order_update", "data": order})
